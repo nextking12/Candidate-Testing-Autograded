a85f9f64b952c1fa4ba5853b67c4974e7bdfb862
Edward King <king8three@gmailcom>
2024-06-21 00:10:52 -0400

    Assignment 1 Part 2 in Progress

--- candidate-testing.js
+++ candidate-testing.js
@@ -12,7 +12,7 @@ let candidateAnswer ="";
 //TODO: Variables for Part 2
 let questions = ["Who was the first American woman in space? " ,"True or false: 5 kilometer == 5000 meters? " , "(5 + 3)/2 * 10 = ? " , "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? " , "What is the minimum crew size for the ISS? " ];
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
-let candidateAnswers = "";
+let candidateAnswers = [];
 
 
 function askForName() {
@@ -22,17 +22,24 @@ candidateName = input.question("What's your name?");
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-candidateAnswer = input.question(question);
+for (i=0; i < questions.length ;i++){
+ candidateAnswers[i] = input.question(questions[i]);
+  if (candidateAnswers[i] === correctAnswers[i]){
+  console.log("Correct!");
+ }
 }
+
+}
+
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-if (correctAnswer === candidateAnswer){
+/*if (candidateAnswers === correctAnswers){
   console.log("Correct!");
 } else {
   console.log("Incorrect, please try again");
-}
+}*/
 
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
