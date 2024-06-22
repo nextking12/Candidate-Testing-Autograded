862378459482b40c8a7a91a3d1411cc85c6b5a94
Edward King <king8three@gmailcom>
2024-06-22 12:58:37 -0400

    Update 4.0 Assignment 1 / Part 2 06222024

--- candidate-testing.js
+++ candidate-testing.js
@@ -27,7 +27,7 @@ function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 
   for (let i = 0; i < questions.length; i++){
-   candidateAnswers[i] = input.question(questions[i]);
+   candidateAnswers[i] = input.question(i + 1 + ") " + questions[i]);
      if (correctAnswers[i] && candidateAnswers[i]){
       console.log(`Your Answer: ${candidateAnswers[i]}`)
       console.log(`Correct Answer: ${correctAnswers[i]}`)
